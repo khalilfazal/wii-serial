601a8178013ccdbadf330b8463af62606538f17a
capncanuck <khalil.fazal.0@gmail.com>
2014-04-04 14:54:47 -0400

    added ino

---
+++ wii_serial/wii_serial.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <wiinunchuck.h>
+
+void setup() {
+  // Initialize the nunchuk
+  nunchuk_setpowerpins();
+  nunchuk_init();
+  nunchuk_send_request();
+  
+  // Open the serial port at 9600 baud
+  Serial.begin(9600);
+  Serial.println("Ready");
+}
+
+void loop() {
+  if (nunchuk_get_data() > 0) {
+    // at this point, nunchuk_buf contains data
+    Serial.print(nunchuk_zbutton());
+    Serial.print(" ");
+    Serial.print(nunchuk_cbutton());
+    Serial.print(" ");
+    Serial.print(nunchuk_joyangle());
+    Serial.print(" ");
+    Serial.print(nunchuk_joyrad());
+    Serial.print(" ");
+    Serial.println(nunchuk_rollangle());
+  }
+}
